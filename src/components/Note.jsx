e379ef9008419460a22807a61f232c24674122c6
gusvicenzi <gusvicenzi12@gmail.com>
2022-01-24 18:32:47 -0300

    First commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Note(){
+    return <div className="note"><h1 className="note">Title</h1>
+    <p className="note">Content</p></div>
+}
+
+export default Note;
