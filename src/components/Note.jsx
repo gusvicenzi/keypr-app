ac9fe80abd6fd8e9733ca1c9d300f6f4807be5aa
gusvicenzi <gusvicenzi12@gmail.com>
2022-02-03 17:15:16 -0300

    start part 3

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function Note(props){
     return (
-        <div className="note"><h1>{props.title}</h1>
-        <p>{props.content}</p></div>
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+        </div>
     )
 }
