9620301d7f6e3464ec886faecd801f1a7a600684
gusvicenzi <gusvicenzi12@gmail.com>
2022-02-04 15:37:15 -0300

    style and animation updates

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 export default function CreateArea(props) {
     const [note, setNote] = useState({title: "", content: ""});
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function expand(){
+        setIsExpanded(true);
+    }
 
     function handleChange(event){
         const {name, value} = event.target;
@@ -15,15 +23,20 @@ export default function CreateArea(props) {
 
     function submitNote(event){
         props.onAdd(note);
+        setNote({title: "", content: ""});
         event.preventDefault();
     }
 
   return (
     <div>
-      <form>
-        <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
-        <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
+      <form className="create-note">
+      {isExpanded && <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />}
+        <textarea onClick={expand} name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows={isExpanded?3:1} />
+        <Zoom in={isExpanded}>
+        <Fab onClick={submitNote}>
+        <AddIcon />
+        </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import KeyIcon from '@mui/icons-material/Key';
 
 function Header(){
-    return (<header><h1>Keypr App</h1></header>);
+    return (<header><h1><KeyIcon/>Keypr</h1></header>);
 }
 
 export default Header;
